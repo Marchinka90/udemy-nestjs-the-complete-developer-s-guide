cc6714ac81b84341688fa0736bc15efd4e0fb158
Marchinka90 <martin_marchev@abv.bg>
2023-10-24 17:37:27 +0300

    Added new routes in controller and methods in Services for CRUD operations on users entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Param,
+    Query, 
+    Controller, 
+    Get, 
+    Post, 
+    Delete, 
+    Patch, 
+    NotFoundException
+} from '@nestjs/common';
+import { CreateUserDto } from './dots/create-user.dto';
+import { UpdateUserDto } from './dots/update-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private userService: UsersService) { }
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto) {
+        this.userService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    async findUser(@Param('id') id: string) {
+        const user = await this.userService.findOne(parseInt(id));
+        if (!user) {
+            return new NotFoundException('User not found');
+        }
+        return user;
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email);
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.userService.update(parseInt(id), body);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id));
+    }
+}
