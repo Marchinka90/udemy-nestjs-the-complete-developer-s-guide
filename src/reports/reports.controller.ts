cd0f4ba085ad1c6ad5b178cfcfe6397cf05a75ef
Marchinka90 <martin_marchev@abv.bg>
2023-10-27 16:41:28 +0300

    Added estimate to reports and first queryBuilder

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,8 +2,10 @@ import {
     Controller,
     Post,
     Patch,
+    Get,
     Body,
     Param,
+    Query,
     UseGuards
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
@@ -14,11 +16,17 @@ import { User } from '../users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApprovedReportDto } from './dtos/approved-report.dto';
-
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportsService: ReportsService) { }
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+        return this.reportsService.createEstimate(query);
+    }
 
     @Post()
     @UseGuards(AuthGuard)
@@ -28,6 +36,7 @@ export class ReportsController {
     }
 
     @Patch('/:id')
+    @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApprovedReportDto ) {
         return this.reportsService.changeApproval(id, body.approved);
     }   
